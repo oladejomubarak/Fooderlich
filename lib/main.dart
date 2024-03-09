4b67be4ad041edaeb5914c8eab27124e2bee3d18
oladejomubarak <oladejomubarakade@gmail.com>
2024-03-09 08:34:48 +0100

    creating fooderlich home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'fooderlich_theme.dart';
 
 void main() {
   // 1
@@ -7,20 +8,22 @@ void main() {
 
 class Fooderlich extends StatelessWidget {
   // 2
-  const Fooderlich({super.key});
+  const Fooderlich({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     // TODO: Create theme
+    final theme = FooderlichTheme.dark();
     // TODO: Apply Home widget
     // 3
     return MaterialApp(
+      theme: theme,
       // TODO: Add theme
       title: 'Fooderlich',
       // 4
       home: Scaffold(
         // TODO: Style the title
-        appBar: AppBar(title: const Text('Fooderlich'),
-        centerTitle: true),
+        appBar: AppBar(title: Text('Fooderlich',
+            style: theme.textTheme.headline6),centerTitle: true,),
         // TODO: Style the body text
         body: const Center(child: Text('Let\'s get cooking 👩‍🍳')),
       ),

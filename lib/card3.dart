f054208c1e367386ed8d0f0c59120f3676c6bfd4
oladejomubarak <oladejomubarakade@gmail.com>
2024-03-10 23:22:54 +0100

    creating recipe cards

--- lib/card3.dart
+++ lib/card3.dart
@@ -6,6 +6,7 @@ class Card3 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
+      // TODO 5: add dark overlay BoxDecoration
         child: Container(
         constraints: const BoxConstraints.expand(width: 350,height: 450),
     decoration: const BoxDecoration(
@@ -15,9 +16,104 @@ class Card3 extends StatelessWidget {
     ),
     child: Stack(
     children: [
-    // TODO 5: add dark overlay BoxDecoration
+
     // TODO 6: Add Container, Column, Icon and Text
+    Container(
+    // 3
+    padding: const EdgeInsets.all(16),
+          // 4
+          child: Column(
+            // 5
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // 6
+              const Icon(Icons.book, color: Colors.white, size: 40),
+              const SizedBox(height: 8),
+              // 8
+              Text(
+                  'Recipe Trends',
+                  style: FooderlichTheme.darkTextTheme.headline2),
+              // 9
+              const SizedBox(height: 30),
+            ],
+          ),
+    ),
+              // 7
     // TODO 7: Add Center widget with Chip widget children
+
+      Center(
+        // 11
+        child: Wrap(
+          // 12
+          alignment: WrapAlignment.start,
+          // 13
+          spacing: 12,
+          // 14
+          children: [
+            Chip(
+              label: Text('Healthy',
+                  style: FooderlichTheme.darkTextTheme.bodyLarge),
+              backgroundColor: Colors.black.withOpacity(0.7),
+              onDeleted: () {
+                print('delete');
+              },
+            ),
+            Chip(
+              label: Text('Vegan',
+                  style: FooderlichTheme.darkTextTheme.bodyLarge),
+              backgroundColor:Colors.black.withOpacity(0.7),
+              onDeleted: () {
+                print('delete');
+              },
+            ),
+            Chip(
+              label: Text('Carrots',
+                  style: FooderlichTheme.darkTextTheme.bodyLarge),
+              backgroundColor:Colors.black.withOpacity(0.7),
+            ),
+            Chip(
+              label: Text('Apple',
+                  style: FooderlichTheme.darkTextTheme.bodyLarge),
+              backgroundColor:Colors.black.withOpacity(0.7),
+            ),
+            Chip(
+              label: Text('Mango',
+                  style: FooderlichTheme.darkTextTheme.bodyLarge),
+              backgroundColor:Colors.black.withOpacity(0.7),
+            ),
+            Chip(
+              label: Text('Orange',
+                  style: FooderlichTheme.darkTextTheme.bodyLarge),
+              backgroundColor:Colors.black.withOpacity(0.7),
+            ),
+            Chip(
+              label: Text('Banana',
+                  style: FooderlichTheme.darkTextTheme.bodyLarge),
+              backgroundColor:Colors.black.withOpacity(0.7),
+            ),
+            Chip(
+              label: Text('Beans',
+                  style: FooderlichTheme.darkTextTheme.bodyLarge),
+              backgroundColor:Colors.black.withOpacity(0.7),
+            ),
+            Chip(
+              label: Text('Potato',
+                  style: FooderlichTheme.darkTextTheme.bodyLarge),
+              backgroundColor:Colors.black.withOpacity(0.7),
+            ),
+            Chip(
+              label: Text('Strawberry',
+                  style: FooderlichTheme.darkTextTheme.bodyLarge),
+              backgroundColor:Colors.black.withOpacity(0.7),
+            ),
+            Chip(
+              label: Text('Cocoa',
+                  style: FooderlichTheme.darkTextTheme.bodyLarge),
+              backgroundColor:Colors.black.withOpacity(0.7),
+            ),
+          ],
+        ),
+      ),
     ],
     ),
     ),

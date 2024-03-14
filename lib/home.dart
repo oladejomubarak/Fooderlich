79457da151e4c4cab0cd4e087c393b0213593088
oladejomubarak <oladejomubarakade@gmail.com>
2024-03-14 12:34:58 +0100

    building mutiple screens for recipe exploration

--- lib/home.dart
+++ lib/home.dart
@@ -1,57 +1,82 @@
 import 'package:flutter/material.dart';
-import 'card1.dart';
-import 'card2.dart';
-import 'card3.dart';
+import 'components/card1.dart';
+import 'components/card2.dart';
+import 'components/card3.dart';
+import 'models/explore_recipe.dart';
 
 class Home extends StatefulWidget {
-  const Home({Key? key}) : super(key: key);
+  const Home({super.key});
+
   @override
-  _HomeState createState() => _HomeState();
+  HomeState createState() => HomeState();
 }
-class _HomeState extends State<Home> {
+
+class HomeState extends State<Home> {
   int _selectedIndex = 0;
-// 8
+
   static List<Widget> pages = <Widget>[
-    const Card1(),
-    const Card2(),
-    const Card3(),
+    Card1(
+        recipe: ExploreRecipe(
+            authorName: 'Ray Wenderlich',
+            title: 'The Art of Dough',
+            subtitle: 'Editor\'s Choice',
+            message: 'Learn to make the perfect bread.',
+            backgroundImage: 'assets/mag1.png')),
+    Card2(
+        recipe: ExploreRecipe(
+            authorName: 'Mike Katz',
+            role: 'Smoothie Connoisseur',
+            profileImage: 'assets/profile_pics/person_katz.jpeg',
+            title: 'Recipe',
+            subtitle: 'Smoothies',
+            backgroundImage: 'assets/magazine_pics/mag2.png')),
+    Card3(
+        recipe: ExploreRecipe(
+            title: 'Vegan Trends',
+            tags: [
+              'Healthy', 'Vegan', 'Carrots', 'Greens', 'Wheat',
+              'Pescetarian', 'Mint', 'Lemongrass',
+              'Salad', 'Water'
+            ],
+            backgroundImage: 'assets/magazine_pics/mag3.png')),
   ];
-// 9
+
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
     });
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          title: Text(
-              'Fooderlich',
-              // 2
-              style: Theme
-                  .of(context)
-                  .textTheme
-                  .titleLarge),
-      centerTitle: true,),
-        body: pages[_selectedIndex],
-        bottomNavigationBar: BottomNavigationBar(
-          // 5
-            selectedItemColor: Theme.of(context).textSelectionTheme.selectionColor,
-            currentIndex: _selectedIndex,
-            onTap: _onItemTapped, // 6
-            items: <BottomNavigationBarItem>[
-              const BottomNavigationBarItem(
-                  icon: Icon(Icons.card_giftcard),
-                  label: 'Card'),
-              const BottomNavigationBarItem(
-                  icon: Icon(Icons.card_giftcard),
-                  label: 'Card2'),
-              const BottomNavigationBarItem(
-                  icon: Icon(Icons.card_giftcard),
-                  label: 'Card3'),
-            ]
-        )
+        title: Text(
+          'Fooderlich',
+          style: Theme.of(context).textTheme.titleLarge,
+        ),
+        centerTitle: true,
+      ),
+      body: pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Theme.of(context).textSelectionTheme.selectionColor,
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.explore),
+            label: 'Explore',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.book),
+            label: 'Recipes',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list),
+            label: 'To Buy',
+          ),
+        ],
+      ),
     );
   }
 }

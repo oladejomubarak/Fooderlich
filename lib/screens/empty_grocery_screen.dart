a389bb9be458113726d050fbaa0cabb59828790b
oladejomubarak <oladejomubarakade@gmail.com>
2024-03-18 16:47:16 +0100

    working on TODO list tab

--- lib/screens/empty_grocery_screen.dart
+++ lib/screens/empty_grocery_screen.dart
@@ -4,6 +4,41 @@ class EmptyGroceryScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO 3: Replace and add layout widgets
-    return Container(color: Colors.purple);
+    // 1
+    return Padding(
+        padding: const EdgeInsets.all(30.0),
+    // 2
+    child: Center(
+    // 3
+    child: Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+    // TODO 4: Add empty image
+    AspectRatio(
+    aspectRatio: 1 / 1,
+    child: Image.asset('assets/fooderlich_assets/empty_list.png'),),
+    // TODO 5: Add empty screen title
+      const SizedBox(height: 8.0),
+      const Text('No Groceries', style: TextStyle(fontSize: 21.0),),
+    // TODO 6: Add empty screen subtitle
+      const SizedBox(height: 16.0),
+      const Text(
+        'Shopping for ingredients?\n'
+            'Tap the + button to write them down!',
+        textAlign: TextAlign.center,
+      ),
+      // TODO 7: Add browse recipes button
+      MaterialButton(
+        textColor: Colors.white,
+        child: const Text('Browse Recipes'),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30.0),),
+        color: Colors.green,
+        onPressed: () {
+          print("Go to Recipe Tab");
+          // TODO 8: Go to Recipes Tab
+        },),
+    ],),),
+    );
   }
 }

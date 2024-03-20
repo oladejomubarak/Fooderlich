ce2d7b5bb5ea5f832b34d0b77c2064903b6f3923
oladejomubarak <oladejomubarakade@gmail.com>
2024-03-20 11:22:57 +0100

    adding items to screen

---
+++ lib/screens/grocery_item_screen.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
+import 'package:intl/intl.dart';
+import 'package:uuid/uuid.dart';
+import '../models/models.dart';
+
+class GroceryItemScreen extends StatefulWidget {
+  // 1
+  final Function(GroceryItem) onCreate;
+  // 2
+  final Function(GroceryItem) onUpdate;
+  // 3
+  final GroceryItem? originalItem;
+  // 4
+  final bool isUpdating;
+  const GroceryItemScreen(
+      {Key? key, required this.onCreate, required this.onUpdate,
+        this.originalItem,})
+      : isUpdating = (originalItem != null),
+        super(key: key);
+  @override
+  _GroceryItemScreenState createState() =>
+      _GroceryItemScreenState();
+}
+class _GroceryItemScreenState extends State<GroceryItemScreen> {
+  // TODO: Add grocery item screen state properties
+  final _nameController = TextEditingController();
+  String _name = '';
+  Importance _importance = Importance.low;
+  DateTime _dueDate = DateTime.now();
+  TimeOfDay _timeOfDay = TimeOfDay.now();
+  Color _currentColor = Colors.green;
+  int _currentSliderValue = 0;
+
+  @override
+  void initState() {
+    final originalItem = widget.originalItem;
+    // 1
+    if (originalItem != null) {
+      _nameController.text = originalItem.name!;
+      _name = originalItem.name!;
+      _currentSliderValue = originalItem.quantity!;
+      _importance = originalItem.importance!;
+      _currentColor = originalItem.color!;
+      final date = originalItem.date;
+      _timeOfDay = TimeOfDay(hour: date!.hour, minute: date.minute,);
+      _dueDate = date;
+    }
+    // 2
+    _nameController.addListener(() {
+      setState(() {
+        _name = _nameController.text;
+      });
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO 12: Add GroceryItemScreen Scaffold
+    return Container(color: Colors.orange);
+  }
+}
